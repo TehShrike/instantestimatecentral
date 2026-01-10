7b8ed153c5f4c20dbec40c659eaae66689f20388
Josh Duff <me@JoshDuff.com>
2026-01-09 22:30:25 -0600

    uh, log I guess?

--- embed/dufftreeservice/all_services.svelte
+++ embed/dufftreeservice/all_services.svelte
@@ -61,6 +61,9 @@
 
 	let current_tab_identifier = $state<ServiceProgrammaticName | null>(null)
 
+	// svelte-ignore state_referenced_locally
+	console.log('initial_tab_identifier', initial_tab_identifier)
+
 	let pricing_args = $state({
 		limb_removal: get_limb_removal_initial_args(),
 		tree_removal: get_tree_removal_initial_args(),
